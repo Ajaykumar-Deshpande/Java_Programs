8a13b1f05865601e58930c1c53e68823d2caff5b
Ajaykumar-Deshpande <ajaydeshpandexworkz@gmail.com>
2024-12-04 22:53:45 +0530

    Collections

---
+++ Collection12/src/com/xworkz/collection/dto/DetergentDTO.java
@@ -0,0 +1,55 @@
+package com.xworkz.collection.dto;
+
+public class DetergentDTO  {
+
+
+        private String brand;
+        private double pricePerKg;
+        private int quantity;
+        private double discount;
+        private String soldBy;
+
+        public DetergentDTO(){
+            System.out.println("no-args constructor");
+        }
+
+        public DetergentDTO(String brand, double pricePerKg, int quantity, double discount, String soldBy) {
+            this.brand = brand;
+            this.pricePerKg = pricePerKg;
+            this.quantity = quantity;
+            this.discount = discount;
+            this.soldBy = soldBy;
+        }
+
+        public String getBrand() {
+            return brand;
+        }
+
+        public double getPricePerKg() {
+            return pricePerKg;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public double getDiscount() {
+            return discount;
+        }
+
+        public String getSoldBy() {
+            return soldBy;
+        }
+
+        @Override
+        public String toString() {
+            return "DetergentDTO{" +
+                    "brand='" + brand + '\'' +
+                    ", pricePerKg=" + pricePerKg +
+                    ", quantity=" + quantity +
+                    ", discount=" + discount +
+                    ", soldBy='" + soldBy + '\'' + '}';
+}
+    }
+
+
---
+++ Collection12/src/com/xworkz/collection/runner/DetergentRunner.java
@@ -0,0 +1,31 @@
+package com.xworkz.collection.runner;
+
+import com.xworkz.collection.dto.DetergentDTO;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class DetergentRunner {
+    public static void main(String[] args) {
+        Collection<DetergentDTO> ref=new ArrayList<DetergentDTO>();
+        ref.add(new DetergentDTO("surf excel",55.0,1,10.0,"Amazon"));
+        ref.add(new DetergentDTO("tiered",90.0,2,5.00,"flipcart") );
+        ref.add(new DetergentDTO("rin",56.0,1,15.00,"Marketing") );
+        ref.add(new DetergentDTO("arial",70.0,1,20.0,"BIG Bascket") );
+        ref.add(new DetergentDTO("wheel", 60.0, 5, 8.0, "Flipkart") );
+        ref.add(new DetergentDTO("ghadi", 50.0, 4, 12.0, "JioMart") );
+        ref.add(new DetergentDTO("nirma", 40.0, 3, 7.0, "BigBasket") );
+        ref.add(new DetergentDTO("henko", 110.0, 7, 10.0, "Amazon") );
+        ref.add(new DetergentDTO("mr. White", 120.0, 5, 18.0, "Flipkart") );
+        ref.add(new DetergentDTO("sunlight", 80.0, 6, 9.0, "BigBasket") );
+
+        ref.stream().forEach(ref1-> System.out.println(ref1));
+        System.out.println("====================");
+        ref.stream().sorted((a,b)->a.getBrand().compareTo(b.getBrand())).forEach(ref2-> System.out.println(ref2));
+        System.out.println("============================");
+        ref.stream().sorted((a,b)->Integer.compare(b.getQuantity(),a.getQuantity())).forEach(ref3-> System.out.println(ref3));
+
+
+    }
+
+}
